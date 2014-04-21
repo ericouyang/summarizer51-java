d800bb0f3f8877038c83bff42fcd7b590973d47e
Eric Ouyang <contact@ericouyang.com>
2014-04-20 23:13:03 -0400

    added files

---
+++ src/Summarizer51.java
@@ -0,0 +1,21 @@
+/**
+ * Summarizer51 
+ * 
+ * Final project for Computer Science 51
+ * 
+ * Spring 2014
+ * 
+ * @author Eric Ouyang <eouyang@college.harvard.edu>
+ * @author Frederick Widjaja <fwidjaja@college.harvard.edu>
+ * 
+ * @version 0.1
+ *
+ */
+public class Summarizer51 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
