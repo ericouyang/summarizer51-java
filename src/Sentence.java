9266a28d5f6a0113abc8c9aa170eafb30fee86a9
Frederick Widjaja <frederick_widjaja@hotmail.com>
2014-04-30 23:17:00 -0400

    Made sentence usable in HashMap

--- src/Sentence.java
+++ src/Sentence.java
@@ -1,18 +1,33 @@
-
 public class Sentence {
     private final String original;
     private final String[] words;
-    
+
     public Sentence(String s) {
-	original = s;
-	words = Parser.parseWords(s);
+        original = s;
+        words = Parser.parseWords(s);
     }
-    
+
     public String getOriginal() {
-	return original;
+        return original;
     }
-    
+
     public String[] getWords() {
-	return words;
+        return words;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (o instanceof Sentence) {
+            Sentence s = (Sentence) o;
+            return original.equals(s.original);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return original.hashCode();
     }
 }
