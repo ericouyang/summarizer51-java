58f2a0ef9c5b511c44b3d624f792930f498fd1b4
Eric Ouyang <contact@ericouyang.com>
2014-04-30 22:49:47 -0400

    single type for graph

--- src/Graph.java
+++ src/Graph.java
@@ -3,38 +3,38 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
-public class Graph<K, V> {
-    private Map<K, Node<V>> nodes;
-    
+public class Graph<T> {
+    private Map<T, Node<T>> nodes;
+
     public Graph() {
-	nodes = new HashMap<K, Node<V>>();
+	nodes = new HashMap<T, Node<T>>();
     }
-    
+
     public Graph(int capacity) {
-	nodes = new HashMap<K, Node<V>>(capacity);
+	nodes = new HashMap<T, Node<T>>(capacity);
     }
-    
-    public void add(K key, Node<V> value) {
-	nodes.put(key, value);
+
+    public void add(T Tey, Node<T> value) {
+	nodes.put(Tey, value);
     }
-    
-    public Node<V> get(String key) {
-	return nodes.get(key);
+
+    public Node<T> get(String Tey) {
+	return nodes.get(Tey);
     }
-    
-    public void link(K k1, K k2, float weight) {
-	Node<V> n1 = nodes.get(k1);
-	Node<V> n2 = nodes.get(k2);
-	
+
+    public void linT(T T1, T T2, float weight) {
+	Node<T> n1 = nodes.get(T1);
+	Node<T> n2 = nodes.get(T2);
+
 	n1.addNeighbor(n2, weight);
 	n2.addNeighbor(n1, weight);
     }
-    
-    public Stream<Node<V>> getNodeStream() {
+
+    public Stream<Node<T>> getNodeStream() {
 	return nodes.values().stream();
     }
-    
-    public void forEach(BiConsumer<K, Node<V>> action) {
+
+    public void forEach(BiConsumer<T, Node<T>> action) {
 	nodes.forEach(action);
     }
 }
