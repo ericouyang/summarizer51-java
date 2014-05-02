fb82780c128e8041599b0ceac399c3b771204f3b
Frederick Widjaja <frederick_widjaja@hotmail.com>
2014-05-02 16:54:44 -0400

    NGram Stuff

--- src/algorithms/textrank/TextRankKeywords.java
+++ src/algorithms/textrank/TextRankKeywords.java
@@ -1,8 +1,11 @@
 package algorithms.textrank;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import types.Graph;
 import types.NGram;
@@ -15,109 +18,125 @@ public class TextRankKeywords extends TextRank implements KeywordExtractor {
 
     private final Word[] words;
     private final Graph<Word> wordGraph;
-    private final Graph<NGram> nGramGraph;
     private final int numKeywords;
 
     public TextRankKeywords(Word[] w, int n) {
-	words = w;
-	wordGraph = new Graph<Word>();
-	nGramGraph = new Graph<NGram>();
-	numKeywords = n;
+        words = w;
+        wordGraph = new Graph<Word>();
+        numKeywords = n;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public String[] getKeywords() {
-	for (int i = 0; i < words.length; i++) {
-	    Word w = words[i];
-	    if (isCandidate(w)) {
-		PositionNode<Word> n = (PositionNode<Word>) wordGraph.get(w);
-		if (n == null)
-		    wordGraph.add(w, new PositionNode<Word>(wordGraph, w, i));
-		else
-		    n.addIndex(i);
-	    }
-	}
+        for (int i = 0; i < words.length; i++) {
+            Word w = words[i];
+            if (isCandidate(w)) {
+                PositionNode<Word> n = (PositionNode<Word>) wordGraph.get(w);
+                if (n == null) {
+                    wordGraph.add(w, new PositionNode<Word>(wordGraph, w, i));
+                } else {
+                    n.addIndex(i);
+                }
+            }
+        }
 
-	calculateRanks(wordGraph);
+        calculateRanks(wordGraph);
 
-	System.out.println(Arrays.toString(wordGraph.getRankedNodes()
-		.limit(numKeywords).map(n -> n.getContent().getWord())
-		.toArray(String[]::new)));
-
-	calculateNGramGraph(wordGraph.getThreshold(numKeywords * 5));
-
-	calculateRanks(nGramGraph);
-
-	return nGramGraph.getRankedNodes().limit(numKeywords)
-		.map(n -> n.getContent().toString()).toArray(String[]::new);
+        return getKeyNGrams();
     }
 
-    private void calculateNGramGraph(double threshold) {
-	int pos = 0;
-	List<Word> nGramWords = new LinkedList<>();
-	for (Word word : words) {
-	    Graph<Word>.Node n = wordGraph.get(word);
-	    if (n != null && nGramWords.size() < MAX_NGRAM_LENGTH
-		    && n.getRank() >= threshold) {
-		nGramWords.add(word);
-	    } else {
-		if (!nGramWords.isEmpty()) {
-		    Word[] words = nGramWords.toArray(new Word[nGramWords
-			    .size()]);
-		    addNGram(pos++, words);
-		    nGramWords.clear();
-		}
-		if (isCandidate(word))
-		    addNGram(pos++, word);
-	    }
-	}
-	if (!nGramWords.isEmpty()) {
-	    Word[] words = nGramWords.toArray(new Word[nGramWords.size()]);
-	    addNGram(pos++, words);
-	    nGramWords.clear();
-	}
+    private String[] getKeyNGrams() {
+        Word[] keywords = wordGraph.getRankedNodes().limit(numKeywords).map(n -> n.getContent())
+                .toArray(Word[]::new);
+        System.out.println(Arrays.toString(keywords));
+        double threshold = wordGraph.getThreshold(numKeywords * 2);
+        Set<NGram> keyNGrams = new HashSet<NGram>();
+        List<Word> nGramWords = new LinkedList<>();
+        boolean containsKeyword = false;
+        for (Word word : words) {
+            Graph<Word>.Node n = wordGraph.get(word);
+            if (n != null && nGramWords.size() < MAX_NGRAM_LENGTH && n.getRank() >= threshold) {
+                nGramWords.add(word);
+                if (!containsKeyword) {
+                    for (Word keyword : keywords) {
+                        if (word.equals(keyword)) {
+                            containsKeyword = true;
+                            break;
+                        }
+                    }
+                }
+            } else if (!nGramWords.isEmpty()) {
+                if (containsKeyword) {
+                    addNGram(keyNGrams, nGramWords);
+                }
+                nGramWords.clear();
+                containsKeyword = false;
+            }
+        }
+        if (!nGramWords.isEmpty() && containsKeyword) {
+            addNGram(keyNGrams, nGramWords);
+        }
+        return keyNGrams.stream().map(n -> n.toString()).limit(numKeywords).toArray(String[]::new);
     }
 
-    @SuppressWarnings("unchecked")
-    private void addNGram(int pos, Word... words) {
-	NGram nGram = new NGram(words);
-	System.out.println("NGram: " + nGram);
-	PositionNode<NGram> nGramNode = (PositionNode<NGram>) nGramGraph
-		.get(nGram);
-	if (nGramNode == null) {
-	    nGramGraph.add(nGram, new PositionNode<NGram>(nGramGraph,
-		    new NGram(words), pos++));
-	} else {
-	    nGramNode.addIndex(pos++);
-	}
+    private void addNGram(Set<NGram> s, List<Word> words) {
+        NGram nGram = new NGram(words.toArray(new Word[words.size()]));
+        if (!s.contains(nGram)) {
+            Iterator<NGram> i = s.iterator();
+            while (i.hasNext()) {
+                NGram n = i.next();
+                if (n.contains(nGram)) {
+                    return;
+                } else if (nGram.contains(n)) {
+                    i.remove();
+                    s.add(nGram);
+                    return;
+                }
+            }
+            s.add(nGram);
+        }
     }
 
     private boolean isCandidate(Word w) {
-	return w.getTag().equals("JJ") || w.getTag().charAt(0) == 'N';
+        return w.getTag().equals("JJ") || w.getTag().charAt(0) == 'N';
     }
+
     /*
-     * private String[] combineNodes(Stream<Graph<Word>.Node> words) {
-     * PositionNode<Word>[] wordNodes = words.map(n -> (PositionNode<Word>) n)
-     * .toArray(PositionNode<Word>[]::new);
+     * private void calculateNGramGraph(double threshold) { int pos = 0; List<Word> nGramWords = new
+     * LinkedList<>(); for (Word word : words) { Graph<Word>.Node n = wordGraph.get(word); if (n !=
+     * null && nGramWords.size() < MAX_NGRAM_LENGTH && n.getRank() >= threshold) {
+     * nGramWords.add(word); } else { if (!nGramWords.isEmpty()) { Word[] words =
+     * nGramWords.toArray(new Word[nGramWords.size()]); addNGram(pos++, words); nGramWords.clear();
+     * } if (isCandidate(word)) { addNGram(pos++, word); } } } if (!nGramWords.isEmpty()) { Word[]
+     * words = nGramWords.toArray(new Word[nGramWords.size()]); addNGram(pos++, words);
+     * nGramWords.clear(); } }
+     * 
+     * @SuppressWarnings("unchecked") private void addNGram(int pos, Word... words) { NGram nGram =
+     * new NGram(words); System.out.println("NGram: " + nGram); PositionNode<NGram> nGramNode =
+     * (PositionNode<NGram>) nGramGraph.get(nGram); if (nGramNode == null) { nGramGraph.add(nGram,
+     * new PositionNode<NGram>(nGramGraph, new NGram(words), pos++)); } else {
+     * nGramNode.addIndex(pos++); } }
+     */
+
+    /*
+     * private String[] combineNodes(Stream<Graph<Word>.Node> words) { PositionNode<Word>[]
+     * wordNodes = words.map(n -> (PositionNode<Word>) n) .toArray(PositionNode<Word>[]::new);
      * 
      * List<String> combinedWordNodes = new ArrayList<>();
      * 
-     * for (int i = 0; i < wordNodes.length; i++) { WordNode n1 = wordNodes[i];
-     * if (n1 != null) { for (int j = i + 1; j < wordNodes.length; j++) {
-     * WordNode n2 = wordNodes[j]; if (n2 != null) { int adjacency =
-     * n1.adjacency(n2);
+     * for (int i = 0; i < wordNodes.length; i++) { WordNode n1 = wordNodes[i]; if (n1 != null) {
+     * for (int j = i + 1; j < wordNodes.length; j++) { WordNode n2 = wordNodes[j]; if (n2 != null)
+     * { int adjacency = n1.adjacency(n2);
      * 
-     * if (adjacency != 0) { String combined = (adjacency < 0) ? n1.getContent()
-     * .getWord() + " " + n2.getContent().getWord() : n2 .getContent().getWord()
-     * + " " + n1.getContent().getWord();
+     * if (adjacency != 0) { String combined = (adjacency < 0) ? n1.getContent() .getWord() + " " +
+     * n2.getContent().getWord() : n2 .getContent().getWord() + " " + n1.getContent().getWord();
      * 
      * wordNodes[i] = null; wordNodes[j] = null;
      * 
      * combinedWordNodes.add(combined); }
      * 
-     * } } if (wordNodes[i] != null)
-     * combinedWordNodes.add(wordNodes[i].getContent().getWord()); } }
+     * } } if (wordNodes[i] != null) combinedWordNodes.add(wordNodes[i].getContent().getWord()); } }
      * 
      * return combinedWordNodes.toArray(new String[combinedWordNodes.size()]); }
      */
--- src/types/NGram.java
+++ src/types/NGram.java
@@ -7,39 +7,44 @@ public class NGram {
     private final String phrase;
 
     public NGram(Word... w) {
-	words = w;
+        words = w;
 
-	StringBuffer s = new StringBuffer();
-	for (int i = 0; i < words.length; i++) {
-	    if (i > 0) {
-		s.append(" ");
-	    }
-	    s.append(words[i].getWord());
-	}
-	phrase = s.toString();
+        StringBuffer s = new StringBuffer();
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                s.append(" ");
+            }
+            s.append(words[i].getWord());
+        }
+        phrase = s.toString();
     }
 
     public Word[] getWords() {
-	return words;
+        return words;
+    }
+
+    public boolean contains(NGram n) {
+        return phrase.contains(n.phrase);
     }
 
     @Override
     public String toString() {
-	return phrase;
+        return phrase;
     }
 
+    @Override
     public boolean equals(Object o) {
-	if (o == this) {
-	    return true;
-	} else if (o instanceof NGram) {
-	    NGram nGram = (NGram) o;
-	    return Arrays.deepEquals(words, nGram.words);
-	}
-	return false;
+        if (o == this) {
+            return true;
+        } else if (o instanceof NGram) {
+            NGram nGram = (NGram) o;
+            return Arrays.deepEquals(words, nGram.words);
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
-	return Arrays.hashCode(words);
+        return Arrays.hashCode(words);
     }
 }
