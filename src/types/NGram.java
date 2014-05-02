d7a1a39d8f4cbc023f737e3a689ac36b5c834160
Eric Ouyang <contact@ericouyang.com>
2014-05-02 16:07:30 -0400

    work on keywords

---
+++ src/types/NGram.java
@@ -0,0 +1,45 @@
+package types;
+
+import java.util.Arrays;
+
+public class NGram {
+    private final Word[] words;
+    private final String phrase;
+
+    public NGram(Word... w) {
+	words = w;
+
+	StringBuffer s = new StringBuffer();
+	for (int i = 0; i < words.length; i++) {
+	    if (i > 0) {
+		s.append(" ");
+	    }
+	    s.append(words[i].getWord());
+	}
+	phrase = s.toString();
+    }
+
+    public Word[] getWords() {
+	return words;
+    }
+
+    @Override
+    public String toString() {
+	return phrase;
+    }
+
+    public boolean equals(Object o) {
+	if (o == this) {
+	    return true;
+	} else if (o instanceof NGram) {
+	    NGram nGram = (NGram) o;
+	    return Arrays.deepEquals(words, nGram.words);
+	}
+	return false;
+    }
+
+    @Override
+    public int hashCode() {
+	return Arrays.hashCode(words);
+    }
+}
